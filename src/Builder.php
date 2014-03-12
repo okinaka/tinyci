fd058528dd811167b169721d59aaa962e6463b36
OKINAKA Kenshin <okinakak@yahoo.co.jp>
2014-03-12 17:52:37 +0900

    Add comment.

--- src/Builder.php
+++ src/Builder.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * TinyCI: Very small continuous integration for PHP
+ * Copyright 2014, OKINAKA Kenshin <okinakak@yahoo.co.jp>
+ *
+ * Licensed under The MIT License.
+ * Redistributions of files must retain the above copyright notice.
+ *
+ */
+
 namespace TinyCI;
 
 class Builder
